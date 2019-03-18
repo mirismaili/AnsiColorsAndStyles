281f3f5575192142de14e019ce9925828b9703d9
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2019-03-19 03:00:57 +0330

    First Successful npm module (+ Babel)

---
+++ app/index.js
@@ -0,0 +1,8 @@
+/**
+ * @author <a href="https://mirismaili.github.io">S. Mahdi Mir-Ismaili</a>.
+ * Created on 1397/12/27 (2019/3/18).
+ */
+
+import {RD} from "colored-cli";
+
+console.log(`${RD} Hello`);
---
+++ library/src/main.js
@@ -0,0 +1,30 @@
+/**
+ * @author <a href="https://mirismaili.github.io">S. Mahdi Mir-Ismaili</a>.
+ * Created on 1397/12/26 (2019/3/17).
+ */
+ 
+export const RST = '\x1b[0m'; 	/* Reset 		*/
+export const BLD = '\x1b[1m'; 	/* Bright 		*/
+export const DIM = '\x1b[2m'; 	/* Dim 			*/
+export const UNL = '\x1b[4m'; 	/* Underscore 	*/
+export const BLK = '\x1b[5m'; 	/* Blink 		*/
+export const RVR = '\x1b[7m'; 	/* Reverse 		*/
+export const HDN = '\x1b[8m'; 	/* Hidden 		*/
+
+export const BK = '\x1b[30m';		/* FgBlack 		*/
+export const RD = '\x1b[31m';		/* FgRed 		*/
+export const GR = '\x1b[32m';		/* FgGreen 		*/
+export const YL = '\x1b[33m';		/* FgYellow 	*/
+export const BL = '\x1b[34m';		/* FgBlue 		*/
+export const MG = '\x1b[35m';		/* FgMagenta 	*/
+export const CN = '\x1b[36m';		/* FgCyan 		*/
+export const WT = '\x1b[37m';		/* FgWhite 		*/
+
+export const _BK = '\x1b[40m';	/* BgBlack 		*/
+export const _RD = '\x1b[41m';	/* BgRed 		*/
+export const _GR = '\x1b[42m';	/* BgGreen 		*/
+export const _YL = '\x1b[43m';	/* BgYellow 	*/
+export const _BL = '\x1b[44m';	/* BgBlue 		*/
+export const _MG = '\x1b[45m';	/* BgMagenta 	*/
+export const _CN = '\x1b[46m';	/* BgCyan 		*/
+export const _WT = '\x1b[47m';	/* BgWhite 		*/
