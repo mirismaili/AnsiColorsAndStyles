9990fa043856f8d1a033a2668fd17cbaa6b9ef8e
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2019-03-19 03:46:02 +0330

    v1.0.1: Variants for all colors and styles added.
    Variants:
    1. Single-letter version
    2. Double-letter version
    3. Triple-letter version
    4. Full version

--- library/src/main.js
+++ library/src/main.js
@@ -2,29 +2,147 @@
  * @author <a href="https://mirismaili.github.io">S. Mahdi Mir-Ismaili</a>.
  * Created on 1397/12/26 (2019/3/17).
  */
- 
-export const RST = '\x1b[0m'; 	/* Reset 		*/
-export const BLD = '\x1b[1m'; 	/* Bright 		*/
-export const DIM = '\x1b[2m'; 	/* Dim 			*/
-export const UNL = '\x1b[4m'; 	/* Underscore 	*/
-export const BLK = '\x1b[5m'; 	/* Blink 		*/
-export const RVR = '\x1b[7m'; 	/* Reverse 		*/
-export const HDN = '\x1b[8m'; 	/* Hidden 		*/
+	
+// Colors reference: https://stackoverflow.com/a/41407246/5318303
 
-export const BK = '\x1b[30m';		/* FgBlack 		*/
-export const RD = '\x1b[31m';		/* FgRed 		*/
-export const GR = '\x1b[32m';		/* FgGreen 		*/
-export const YL = '\x1b[33m';		/* FgYellow 	*/
-export const BL = '\x1b[34m';		/* FgBlue 		*/
-export const MG = '\x1b[35m';		/* FgMagenta 	*/
-export const CN = '\x1b[36m';		/* FgCyan 		*/
-export const WT = '\x1b[37m';		/* FgWhite 		*/
+export const RESET      =  '\x1b[0m';
+export const BRIGHT     =  '\x1b[1m';
+export const DIM        =  '\x1b[2m';
+export const UNDERSCORE =  '\x1b[4m';
+export const BLINK      =  '\x1b[5m';
+export const REVERSE    =  '\x1b[7m';
+export const HIDDEN     =  '\x1b[8m';
 
-export const _BK = '\x1b[40m';	/* BgBlack 		*/
-export const _RD = '\x1b[41m';	/* BgRed 		*/
-export const _GR = '\x1b[42m';	/* BgGreen 		*/
-export const _YL = '\x1b[43m';	/* BgYellow 	*/
-export const _BL = '\x1b[44m';	/* BgBlue 		*/
-export const _MG = '\x1b[45m';	/* BgMagenta 	*/
-export const _CN = '\x1b[46m';	/* BgCyan 		*/
-export const _WT = '\x1b[47m';	/* BgWhite 		*/
+export const FG_BLACK   = '\x1b[30m';
+export const FG_RED     = '\x1b[31m';
+export const FG_GREEN   = '\x1b[32m';
+export const FG_YELLOW  = '\x1b[33m';
+export const FG_BLUE    = '\x1b[34m';
+export const FG_MAGENTA = '\x1b[35m';
+export const FG_CYAN    = '\x1b[36m';
+export const FG_WHITE   = '\x1b[37m';
+
+export const BG_BLACK   = '\x1b[40m';
+export const BG_RED     = '\x1b[41m';
+export const BG_GREEN   = '\x1b[42m';
+export const BG_YELLOW  = '\x1b[43m';
+export const BG_BLUE    = '\x1b[44m';
+export const BG_MAGENTA = '\x1b[45m';
+export const BG_CYAN    = '\x1b[46m';
+export const BG_WHITE   = '\x1b[47m';
+//------------------------------------------------------------------/
+
+export const T   = RESET;
+export const RS  = RESET;
+export const RT  = RESET;
+export const RST = RESET;
+
+export const B    = BRIGHT;
+export const BD   = BRIGHT;
+export const BR   = BRIGHT;
+export const BLD  = BRIGHT;
+export const BRT  = BRIGHT;
+export const BOLD = BRIGHT;
+
+export const I   = DIM;
+export const DM  = DIM;
+// ... const DIM = DIM;
+
+export const U         = UNDERSCORE;
+export const UL        = UNDERSCORE;
+export const US        = UNDERSCORE;
+export const UNL       = UNDERSCORE;
+export const UNS       = UNDERSCORE;
+export const UNDERLINE = UNDERSCORE;
+
+export const N   = BLINK;
+export const BN  = BLINK;
+export const BNK = BLINK;
+
+export const V   = REVERSE;
+export const RV  = REVERSE;
+export const RVR = REVERSE;
+
+export const H   = HIDDEN;
+export const HN  = HIDDEN;
+export const HDN = HIDDEN;
+//------------------------------------------------------------------/
+
+export const K       = FG_BLACK;
+export const BK      = FG_BLACK;
+export const BLK     = FG_BLACK;
+export const BLACK   = FG_BLACK;
+
+export const R       = FG_RED;
+export const RD      = FG_RED;
+export const RED     = FG_RED;
+
+export const G       = FG_GREEN;
+export const GR      = FG_GREEN;
+export const GRN     = FG_GREEN;
+export const GREEN   = FG_GREEN;
+
+export const Y       = FG_YELLOW;
+export const YL      = FG_YELLOW;
+export const YLW     = FG_YELLOW;
+export const YELLOW  = FG_YELLOW;
+
+export const E       = FG_BLUE;
+export const BL      = FG_BLUE;
+export const BLU     = FG_BLUE;
+export const BLUE    = FG_BLUE;
+
+export const M       = FG_MAGENTA;
+export const MG      = FG_MAGENTA;
+export const MGN     = FG_MAGENTA;
+export const MAGENTA = FG_MAGENTA;
+
+export const C       = FG_CYAN;
+export const CN      = FG_CYAN;
+export const CYN     = FG_CYAN;
+export const CYAN    = FG_CYAN;
+
+export const W       = FG_WHITE;
+export const WT      = FG_WHITE;
+export const WHT     = FG_WHITE;
+export const WHITE   = FG_WHITE;
+//------------------------------------------------------------------/
+
+export const _K       = BG_BLACK;
+export const _BK      = BG_BLACK;
+export const _BLK     = BG_BLACK;
+export const _BLACK   = BG_BLACK;
+
+export const _R       = BG_RED;
+export const _RD      = BG_RED;
+export const _RED     = BG_RED;
+
+export const _G       = BG_GREEN;
+export const _GR      = BG_GREEN;
+export const _GRN     = BG_GREEN;
+export const _GREEN   = BG_GREEN;
+
+export const _Y       = BG_YELLOW;
+export const _YL      = BG_YELLOW;
+export const _YLW     = BG_YELLOW;
+export const _YELLOW  = BG_YELLOW;
+
+export const _E       = BG_BLUE;
+export const _BL      = BG_BLUE;
+export const _BLU     = BG_BLUE;
+export const _BLUE    = BG_BLUE;
+
+export const _M       = BG_MAGENTA;
+export const _MG      = BG_MAGENTA;
+export const _MGN     = BG_MAGENTA;
+export const _MAGENTA = BG_MAGENTA;
+
+export const _C       = BG_CYAN;
+export const _CN      = BG_CYAN;
+export const _CYN     = BG_CYAN;
+export const _CYAN    = BG_CYAN;
+
+export const _W       = BG_WHITE;
+export const _WT      = BG_WHITE;
+export const _WHT     = BG_WHITE;
+export const _WHITE   = BG_WHITE;
